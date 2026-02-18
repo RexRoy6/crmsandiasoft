9b118676b2b6dd31ed6faa11aafdb30475020c03
Rodrigo Aguilera <rodris9806@gmail.com>
2026-02-18 15:59:40 -0700

    feat[migrations]: migration set up

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import mysql from "mysql2/promise"
+import { drizzle } from "drizzle-orm/mysql2"
+
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL!,
+  connectionLimit: 10
+})
+
+export const db = drizzle(pool)
