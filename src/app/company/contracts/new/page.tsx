bc514b87271dbc5179f04c9c2c1acee14cce0f52
Rodrigo Aguilera <rodris9806@gmail.com>
2026-04-28 18:27:48 -0600

    feat[mod]: mod for serach status of contract

---
+++ src/app/company/contracts/new/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+
+
+
+export default function NewPage() {
+    const [contractId, setContractId] = useState<number | null>(null);
+const [eventId, setEventId] = useState<number | null>(null);
+const [clientId, setClientId] = useState<number | null>(null);
+
+const [loadingDraft, setLoadingDraft] = useState(true);
+
+
+}
